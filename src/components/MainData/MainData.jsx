5423e815d812ccc52cc43fe7fafe30daf124e605
Prashant Sharma <prashant.sharma@thoughts2binary.com>
2024-10-25 23:58:21 +0530

    basic partition layout

--- src/components/MainData/MainData.jsx
+++ src/components/MainData/MainData.jsx
@@ -1,4 +1,9 @@
 function MainData() {
-  return <>MainData</>;
+  return (
+    <div className="basis-9/12 text-black h-full bg-[#f6f6f8] rounded-br-3xl rounded-tr-3xl">
+      MainData
+    </div>
+  );
 }
+
 export default MainData;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,11 @@
 function Sidebar() {
-  return <>Sidebar</>;
+  return (
+    <div
+      className="basis-3/12 flex flex-col items-center text-black h-full rounded-tl-3xl rounded-bl-3xl"
+      style={{ background: "rgba(255,255,255,0.81" }}
+    >
+      Sidebar
+    </div>
+  );
 }
 export default Sidebar;
